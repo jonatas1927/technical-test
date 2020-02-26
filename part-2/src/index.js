e63be919dc01bab0790c27ac843b1fa0aadd16b5
Jônatas Thielke <jonatas1927@gmail.com>
2020-02-26 14:17:41 -0300

    desafio 2

--- part-2/src/index.js
+++ part-2/src/index.js
@@ -1,59 +1,53 @@
 const fs = require('fs');
 const _ = require('lodash')
-const request = require('request');
+const request = require('sync-request');
 
-fs.readFile('../input/input-dump', {}, (error, data) => {
-    if (error)
-        console.log(error)
-    else {
-        let file = Buffer.from(data).toString('ascii').split('\n');
+var data = fs.readFileSync('../input/input-dump')
 
-        var objs = []
+console.log('Leitura do Dump')
+let file = Buffer.from(data).toString('ascii').split('\n');
 
-        file.map((x, index) => {
-            if (x.indexOf('{') > -1)
-                try {
-                    var obj = JSON.parse('{' + x.split('{')[1]);
-                    if (obj)
-                        objs.push(obj)
-                } catch (ex) {
-                    if (index > 0) {
-                        console.log(ex.message)
-                    }
-                }
-        })
+var objs = []
 
-        objs = _.orderBy(objs, ['productId', 'image'], ['asc'])
+file.map((x, index) => {
+    if (x.indexOf('{') > -1)
+        try {
+            var obj = JSON.parse('{' + x.split('{')[1]);
+            if (obj)
+                objs.push(obj)
+        } catch (ex) {
+            if (index > 0) {
+                console.log(ex.message)
+            }
+        }
+})
 
-        let listaObjsTratamento = {};
+objs = _.orderBy(objs, ['productId', 'image'], ['asc'])
 
-        objs.map(x => {
-            listaObjsTratamento[x.productId] = _.get(listaObjsTratamento, x.productId, []);
+let listaObjsTratamento = {};
+
+objs.map((x, index) => {
+    listaObjsTratamento[x.productId] = _.get(listaObjsTratamento, x.productId, []);
+    if (listaObjsTratamento[x.productId].length < 3) {
+        var res = request('GET', x.image)
+        console.log(index)
+        if (res && res.statusCode == 200) {
             if (listaObjsTratamento[x.productId].length < 3) {
-                // console.log(x.image)
-                request(x.image, { json: true }, (err, res, body) => {
-                    if (err) {
-                        // return console.log('erro');
-                    }
-                    if (res && res.statusCode == 200) {
-                        if (listaObjsTratamento[x.productId].length < 3) {
-                            listaObjsTratamento[x.productId].push(x.image)
-                        }
-                    }
-                });
+                listaObjsTratamento[x.productId].push(x.image)
             }
-        })
-        let listaObjsFinal = [];
-
-        Object.keys(listaObjsTratamento).map(x => {
-            listaObjsFinal.push({ productId: x, images: listaObjsTratamento[x] })
-        })
-
-        fs.writeFile("../output", JSON.stringify(listaObjsFinal), function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        });
+        }
     }
 })
+let listaObjsFinal = [];
+
+Object.keys(listaObjsTratamento).map(x => {
+    listaObjsFinal.push({ productId: x, images: listaObjsTratamento[x] })
+})
+
+fs.writeFile("../output", JSON.stringify(listaObjsFinal), function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Arquivo foi Salvo!");
+});
+
